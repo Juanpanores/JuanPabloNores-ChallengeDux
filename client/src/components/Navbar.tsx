b2dee3c87083fa9afc8012c97be28e054b3581a8
Juan Pablo Nores <noresjpn@gmail.com>
2025-04-24 17:27:21 -0300

    applied primeflex

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,12 +4,20 @@ import { Image } from "primereact/image";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.css"; // Import PrimeFlex
 import "./Navbar.css";
 
 export default function Navbar() {
-  const start = (<Image src="/images/logo.png" alt="logo" className="navbar-logo" />)
+  const start = (<Image src="/images/logo.png" alt="logo" className="navbar-logo" />);
 
-  const end = (<i className="pi pi-cog navbar-icon" />)
+  const end = (<i className="pi pi-cog navbar-icon" />);
 
-  return <Menubar model={[]} start={start} end={end} className="navbar-container" />;
+  return (
+    <Menubar
+      model={[]}
+      start={start}
+      end={end}
+      className="navbar-container flex align-items-center justify-content-between"
+    />
+  );
 }
