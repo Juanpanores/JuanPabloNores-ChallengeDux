06c29b4a2651188785677cf713166b7ed61beb91
Juan Pablo Nores <76434355+Juanpanores@users.noreply.github.com>
2025-04-26 16:31:41 -0300

    Revert "Navbar component"

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,9 @@
-import Navbar from "@/components/Navbar";
 import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <>
-      <Navbar /> {/* Add the Navbar component */}
-      <div className={styles.page}>
-        
-      </div>
-    </>
+    <div className={styles.page}>
+      
+    </div>
   );
 }
