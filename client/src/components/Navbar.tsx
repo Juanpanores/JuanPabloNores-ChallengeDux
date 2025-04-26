51b489c897bc2c5b23582459bccca9f111d913bf
Juan Pablo Nores <noresjpn@gmail.com>
2025-04-26 16:24:18 -0300

    small fixes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { Menubar } from "primereact/menubar";
 import { Image } from "primereact/image";
-import "primereact/resources/themes/saga-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-import "primeflex/primeflex.css"; // Import PrimeFlex
-import "./Navbar.css";
+import './Navbar.css'
+
 
 export default function Navbar() {
-  const start = (<Image src="/images/logo.png" alt="logo" className="navbar-logo" />);
+  const start = (
+    <Image
+      src="/images/logo.png"
+      alt="logo"
+      className="h-3rem" /* PrimeFlex class for height */
+    />
+  );
 
-  const end = (<i className="pi pi-cog navbar-icon" />);
+  const end = (
+    <i
+      className="pi pi-cog text-white text-2xl" /* PrimeFlex classes for color and size */
+    />
+  );
 
   return (
-    <Menubar
-      model={[]}
-      start={start}
-      end={end}
-      className="navbar-container flex align-items-center justify-content-between"
-    />
+    <div className="bg-blue-500 h-4rem"> {/* Asegúrate de que la altura esté definida */}
+      <Menubar
+        model={[]}
+        start={start}
+        end={end}
+        className="custom-menubar"
+      />
+    </div>
   );
 }
